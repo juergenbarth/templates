bcc37e606e73866ceb2506a0391de5ad21c77a32
Jay <87374371+juergenbarth@users.noreply.github.com>
2025-12-20 11:03:00 +0100

    Update docker-network.sh

--- script/docker-network.sh
+++ script/docker-network.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ip link add mvl-brg link eth0 type macvlan mode bridge
-ip addr add 192.168.0.63/32 dev mvl-brg
+ip addr add 192.168.0.62/32 dev mvl-brg
 ip link set mvl-brg up
 ip route add 192.168.0.32/27 dev mvl-brg
