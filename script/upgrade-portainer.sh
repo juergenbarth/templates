7cb7856486b7f4913705d7350a7807eedb813c66
Jay <87374371+juergenbarth@users.noreply.github.com>
2025-12-21 12:55:29 +0100

    Create upgrade-portainer.sh

---
+++ script/upgrade-portainer.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+docker stop portainer
+docker rm portainer
+docker pull portainer/portainer-ce:lts
+docker run -d -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:lts
